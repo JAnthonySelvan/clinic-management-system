460f25dc5f0f8118248306a8fdb2ad3b03232d16
j.anthonyselvan <janthonyselvan07@gmail.com>
2026-08-05 04:21:06 +0530

    Fixed admin page error

--- frontend/src/pages/auth/AdminLogin.jsx
+++ frontend/src/pages/auth/AdminLogin.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { login, logout, clearError } from "../../features/auth/authSlice";
 import { SITE_LOGO } from "../../constants/images";
